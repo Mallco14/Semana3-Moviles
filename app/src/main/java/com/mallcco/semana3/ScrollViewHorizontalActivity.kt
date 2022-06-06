95c64d02847f593fbb73e7e0f00a01352d9f0d01
JhonM <jhon@example.com>
2022-06-05 21:07:24 -0500

    first commit

---
+++ app/src/main/java/com/mallcco/semana3/ScrollViewHorizontalActivity.kt
@@ -0,0 +1,12 @@
+package com.mallcco.semana3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class ScrollViewHorizontalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scroll_view_horizontal)
+    }
+}
